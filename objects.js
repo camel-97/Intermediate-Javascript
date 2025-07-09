6c86d1a437e3800460580252e7b7a5082ec470c7
camel-97 <cfmcmillan@icloud.com>
2025-07-09 17:41:15 +0100

    comments added for readability

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
+//Create Library Array
 const myLibrary = []
 
+//Function for book creation
 function Book(title, author, length, read){
     if (!new.target){
         throw Error("You must use the 'new' operator to call the constructor")
@@ -14,6 +16,7 @@ function Book(title, author, length, read){
     }
 }
 
+//Setting form elements to constants
 const title = document.getElementById("title-input");
 const author = document.getElementById("author-input");
 const length = document.getElementById("length-input");
@@ -21,19 +24,19 @@ const read = document.getElementById("read-input");
 const submitBtn = document.getElementById("submit-btn");
 const resetBtn = document.getElementById("reset-btn")
 
-
+//Add book to library function that calls book creation function
 function addBookToLibrary(title, author, length, read) {
     const newBook = new Book(title, author, length, read);
     myLibrary.push(newBook);
 }
 
-document.addEventListener()
-addBookToLibrary(
-    title.value,
-    author.value,
-    length.value,
-    read.value
-);
-
-
+//Event listener to submit button, calling add book function
+submitBtn.addEventListener("click", () => {
+    addBookToLibrary(
+        title.value,
+        author.value,
+        length.value,
+        read.value);
+        console.log(myLibrary)
+});
 
